bf3780870f7776f0f2b86fecd1857c0949ccc161
kahzerx <kahzerx@tutanota.com>
2022-12-06 14:07:55 +0100

    cleanup

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     server_executor: ServerExecutor = ServerExecutor(bot_logger, server)
     server_executor.start()
 
-    client_config: KCPClientConfig = KCPClientConfig(remote="0.0.0.0:25583", listen=":25555", password="OP_PASS")
-    client: SystemHandler = SystemHandler(bot_logger, ServiceMode.CLIENT, client_config)
-
     client_executor: ClientExecutor = ClientExecutor(bot_logger)
     client_executor.start()
+
+    client_config: KCPClientConfig = KCPClientConfig(remote="0.0.0.0:25583", listen=":25555", password="OP_PASS")
+    client: SystemHandler = SystemHandler(bot_logger, ServiceMode.CLIENT, client_config)
 
     client_executor.add_handler(client)
 
