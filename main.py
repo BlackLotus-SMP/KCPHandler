1aec7d565391760695c11f16b1baaa3fb269b107
kahzerx <kahzerx@tutanota.com>
2022-12-11 13:51:33 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.config.kcp_config import KCPServerConfig, KCPClientConfig
 from src.constant import BOT_NAME
-from src.handlers.system import SystemHandler
+from src.handlers.system.system import SystemHandler
 from src.logger.bot_logger import BotLogger
 from src.service.mode import ServiceMode
 from src.thread_executor.client_executor import ClientExecutor
 from src.thread_executor.server_executor import ServerExecutor
 
-if __name__ == '__main__':
+
+def main():
     bot_logger = BotLogger()
     bot_logger.info(f"Started {BOT_NAME} bot")
 
@@ -24,3 +25,7 @@ if __name__ == '__main__':
     client_executor.add_handler(client)
 
     server_executor.join()
+
+
+if __name__ == '__main__':
+    main()
