131d5269d0d112cd5c9288bc26a22a0874e5b410
kahzerx <kahzerx@tutanota.com>
2022-12-25 02:31:38 +0100

    no cov com

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pytest tests --no-header --doctest-modules --junitxml=junit/test-results.xml --cov=com --cov-report=xml --cov-report=html
+pytest tests --no-header --doctest-modules --junitxml=junit/test-results.xml --cov-report=xml --cov-report=html
